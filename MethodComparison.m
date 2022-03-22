472e9048e68f5131d8fbd6a73002750e6c7c98d8
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2022-03-22 16:47:21 -0700

    Refining the method for computing mean density profile
    I think we will settle on N2stable, but need to do a few more tests. Also might consider enabling the wkb stretching for this case, at least as a point of comparison.

--- MethodComparison.m
+++ MethodComparison.m
@@ -1,16 +1,16 @@
 lat0 = 31; lon0 = -59;
-[rho,z,rho0] = MeanDensityProfileFromLatLon(lat0,lon0,DensityMethod.rho);
-if length(rho) < 10
+[rhoOrN2,z,rho0] = MeanDensityProfileFromLatLon(lat0,lon0,DensityMethod.rhoFromN2);
+if length(rhoOrN2) < 10
     error('There are fewer than 10 points.');
 end
 z_out = double(linspace(min(z),max(z),512)');
 nModes = 100;
 nPoints = 101;
-im = InternalModesSpectral(double(rho),double(z),z_out,lat0,'nEVP',512);
+im = InternalModesSpectral(double(rhoOrN2),double(z),z_out,lat0,'nEVP',512);
 im.upperBoundary = UpperBoundary.freeSurface;
 z_g = im.GaussQuadraturePointsForModesAtFrequency(nPoints,0);
 
-im = InternalModesSpectral(double(rho),double(z),z_g,lat0,'nEVP',512,'nModes',nModes);
+im = InternalModesSpectral(double(rhoOrN2),double(z),z_g,lat0,'nEVP',512,'nModes',nModes);
 im.upperBoundary = UpperBoundary.freeSurface;
 im.normalization = Normalization.uMax;
 [F_a,G_a,h_a,k_a,wMaxRatio_a,kConstantRatio_a,omegaConstantRatio_a] = im.ModesAtFrequency(0,'wMax','kConstant','omegaConstant');
@@ -19,21 +19,33 @@ errorFunction = @(x,y) max(abs(x-y),[],1)./max(abs(y),[],1);
 errorTolerance = 1e-2;
 
 methods = cell(1,1);
-methods{1} = DensityMethod.rhoStable;
-methods{2} = DensityMethod.rhoFromN2;
-methods{3} = DensityMethod.rhoStableFromN2;
+% methods{1} = DensityMethod.rhoStable;
+% methods{2} = DensityMethod.rhoFromN2;
+% methods{3} = DensityMethod.rhoStableFromN2;
+methods{1} = DensityMethod.N2;
 
 for iMethod=1:length(methods)
-    [rho,z,rho0] = MeanDensityProfileFromLatLon(lat0,lon0,methods{iMethod});
-    if length(rho) < 10
+    [rhoOrN2,z,rho0] = MeanDensityProfileFromLatLon(lat0,lon0,methods{iMethod});
+    if length(rhoOrN2) < 10
         error('There are fewer than 10 points.');
     end
-    z_out = double(linspace(min(z),max(z),512)');
-    im = InternalModesSpectral(double(rho),double(z),z_out,lat0,'nEVP',512);
-    im.upperBoundary = UpperBoundary.freeSurface;
-    z_g = im.GaussQuadraturePointsForModesAtFrequency(nPoints,0);
+    switch methods{iMethod}
+        case {DensityMethod.N2,DensityMethod.stableN2}
+            z_out = double(linspace(min(z),max(z),512)');
+            N2function = @(zz) interp1(z,rhoOrN2,zz,'linear','extrap');
+            im = InternalModesSpectral(N2function,[min(z_g) max(z_g)],z_out,lat0,'nEVP',512,'N2',1,'rho0',rho0);
+            im.upperBoundary = UpperBoundary.freeSurface;
+            z_g = im.GaussQuadraturePointsForModesAtFrequency(nPoints,0);
 
-    im = InternalModesSpectral(double(rho),double(z),z_g,lat0,'nEVP',512,'nModes',nModes);
+            im = InternalModesSpectral(N2function,[min(z_g) max(z_g)],z_g,lat0,'nEVP',512,'nModes',nModes,'N2',1,'rho0',rho0);
+        otherwise
+            z_out = double(linspace(min(z),max(z),512)');
+            im = InternalModesSpectral(double(rhoOrN2),double(z),z_out,lat0,'nEVP',512);
+            im.upperBoundary = UpperBoundary.freeSurface;
+            z_g = im.GaussQuadraturePointsForModesAtFrequency(nPoints,0);
+
+            im = InternalModesSpectral(double(rhoOrN2),double(z),z_g,lat0,'nEVP',512,'nModes',nModes);
+    end
     im.upperBoundary = UpperBoundary.freeSurface;
     im.normalization = Normalization.uMax;
     [F,G,h,k,wMaxRatio,kConstantRatio,omegaConstantRatio] = im.ModesAtFrequency(0,'wMax','kConstant','omegaConstant');
