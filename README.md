9fd0f07c82dcbe22b011dec92941c5d5c513872b
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2022-04-25 15:07:08 -0700

    Updating readme

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Vertical Mode Atlas
+Vertical Mode Atlas
+==============
+
 Global ocean atlas of the vertical modes, including equivalent depth and Rossby number
+
+Quick start
+------------
+
+The atlas is initialized by pointing to the NetCDF data that houses the vertical modes and stratification. So, on my machine the one-degree atlas is initialized with,
+```matlab
+atlas = VerticalModeAtlas('/Volumes/Samsung_T5/VerticalModeAtlas/VerticalModeAtlas-01.nc');
+```
+
+After that, you can then query the Atlas for data or to make a few plots. For example,
+```matlab
+    atlas.ShowLowestModes(lat0,lon0)
+    atlas.ShowLowestWKBModes(lat0,lon0)
+```
+will plot the lowest four modes for a given lat/lon from the numerical solutions and the WKB approximated solutions.
+
+The squared buoyancy frequency can also be fetched,
+```matlab
+    [N2,z] = atlas.N2(lat0,lon0);
+```
+
+Various approximations to the vertical structure functions are fetched as follows,
+```matlab
+j_star = 3;
+H_norm = 1/sum((j_star+(1:3000)).^(-5/2));
+H = @(j) H_norm*(j_star+j).^(-5/2);
+
+[Phi_hs,Gamma_hs,z] = atlas.VerticalStructureFunctionsWithDistribution(lat0,lon0,H,'exact-hydrostatic');
+[Phi_wkb,Gamma_wkb] = atlas.VerticalStructureFunctionsWithDistribution(lat0,lon0,H,'wkb-hydrostatic');
+[Phi_igm,Gamma_igm] = atlas.VerticalStructureFunctionsWithDistribution(lat0,lon0,H,'igm');
+[Phi_gm,Gamma_gm] = atlas.VerticalStructureFunctionsWithDistribution(lat0,lon0,H,'gm');
+```
+
+Note that you can pass any `H(j)` (as a function handle) to set your own vertical spectrum.
 
--- examples/ShowWKBModes.m
+++ examples/ShowWKBModes.m
@@ -3,12 +3,14 @@ scaleFactor = 1;
 LoadFigureDefaults
 
 lat0=45; lon0=-150; % Ocean Station PAPA
-lat0=-39; lon0=10.5; % Agulhas
+% lat0=-39; lon0=10.5; % Agulhas
+lat0=34; lon0=-70; % Woods Hole, Site D
 
-atlas = VerticalModeAtlas('/Volumes/MoreStorage/Data/VerticalModeAtlas/VerticalModeAtlas-01.nc');
+% atlas = VerticalModeAtlas('/Volumes/MoreStorage/Data/VerticalModeAtlas/VerticalModeAtlas-01.nc');
+atlas = VerticalModeAtlas('/Volumes/Samsung_T5/VerticalModeAtlas/VerticalModeAtlas-01.nc');
 
-atlas.ShowLowestModes(lat0,lon0)
-atlas.ShowLowestWKBModes(lat0,lon0)
+% atlas.ShowLowestModes(lat0,lon0)
+% atlas.ShowLowestWKBModes(lat0,lon0)
 
 j_star = 3;
 H_norm = 1/sum((j_star+(1:3000)).^(-5/2));
@@ -19,23 +21,53 @@ H = @(j) H_norm*(j_star+j).^(-5/2);
 [Phi_igm,Gamma_igm] = atlas.VerticalStructureFunctionsWithDistribution(lat0,lon0,H,'igm');
 [Phi_gm,Gamma_gm] = atlas.VerticalStructureFunctionsWithDistribution(lat0,lon0,H,'gm');
 
-figure
+shouldUseYlog = 1;
+if shouldUseYlog == 1
+    z(end) = -1;
+    ylimits = [min(z) -10];
+else
+    ylimits = [min(z) max(z)];
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% plots
+
+FigureSize = [50 50 figure_width_1col+8 225*scaleFactor];
+
+fig1 = figure('Units', 'points', 'Position', FigureSize);
+set(gcf,'PaperPositionMode','auto')
+set(gcf, 'Color', 'w');
+fig1.PaperUnits = 'points';
+fig1.PaperPosition = FigureSize;
+fig1.PaperSize = [FigureSize(3) FigureSize(4)];
+
 subplot(1,2,1)
-plot(Phi_wkb./Phi_hs,z,'LineWidth',2), hold on
-plot(Phi_igm./Phi_hs,z,'LineWidth',2)
-plot(Phi_gm./Phi_hs,z,'LineWidth',2)
+plot(Phi_wkb./Phi_hs,z,'LineWidth',2*scaleFactor), hold on
+plot(Phi_igm./Phi_hs,z,'LineWidth',2*scaleFactor)
+plot(Phi_gm./Phi_hs,z,'LineWidth',2*scaleFactor)
 plot([1 1],[z(1) z(end)],'k--')
-ylim([min(z) max(z)])
+ylog
+ylim(ylimits)
+ylabel('depth (m)', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
+title('$\Phi^\textrm{approx}(z)/\Phi(z)$','Interpreter','LaTex', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
+set( gca, 'FontSize', figure_axis_tick_size);
 
 subplot(1,2,2)
-plot(Gamma_wkb./Gamma_hs,z,'LineWidth',2), hold on
-plot(Gamma_igm./Gamma_hs,z,'LineWidth',2)
-plot(Gamma_gm./Gamma_hs,z,'LineWidth',2)
+plot(Gamma_wkb./Gamma_hs,z,'LineWidth',2*scaleFactor), hold on
+plot(Gamma_igm./Gamma_hs,z,'LineWidth',2*scaleFactor)
+plot(Gamma_gm./Gamma_hs,z,'LineWidth',2*scaleFactor)
 plot([1 1],[z(1) z(end)],'k--')
-ylim([min(z) max(z)])
+ylog
+ylim(ylimits)
 xlim([0 3])
-legend('wkb','igm','gm')
+title('$\Gamma^\textrm{approx}(z)/\Gamma(z)$','Interpreter','LaTex', 'FontSize', figure_axis_label_size, 'FontName', figure_font);
 
+legend('wkb','igm','gm')
+set( gca, 'FontSize', figure_axis_tick_size);
+
+packfig(1,2)
+
+print('StructureFunctionApproximations.eps','-depsc2')
 
 % figure
 % subplot(1,2,1)
